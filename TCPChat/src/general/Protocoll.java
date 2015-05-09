f458d904825e15b7347fa81e62f3d521580a8aa1
Dirk <Dirk@192.168.19.102>
2015-05-09 16:54:33 +0200

    unnötig

--- TCPChat/src/general/Protocoll.java
+++
@@ -1,9 +0,0 @@
-package general;
-
-public class Protocoll {
-	public final static String DATA_PACK_START = "";
-	public final static String DATA_PACK_END = "END";
-	
-	private Protocoll() {// let nobody instantiate this Class
-	}
-}
